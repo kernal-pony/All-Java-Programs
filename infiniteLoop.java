87bafa7f37ed51798707252143a2ee114efda91b
kernal-pony <arnab.lalaofficial@gmail.com>
2024-06-09 14:38:59 +0530

    example of Infinite loop using do-while

---
+++ infiniteLoop.java
@@ -0,0 +1,10 @@
+public class infiniteLoop 
+{
+    public static void main(String args[])
+    {
+        do{
+            System.out.println("This is an example of Infinite Loop!!!");
+        }while(true);
+    }
+    
+}
