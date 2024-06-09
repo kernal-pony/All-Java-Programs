22ef3b3c6df60cdfc8e62d05e0baedb67578620a
kernal-pony <arnab.lalaofficial@gmail.com>
2024-06-09 13:02:12 +0530

    function to find average of 3 numbers

---
+++ funcAverage.java
@@ -0,0 +1,23 @@
+//Enter 3 numbers from the user & make a function to print their average.
+import java.util.*;
+public class funcAverage 
+{
+    public static double checkAvearge(double a,double b,double c)
+    {
+        double x=a,y=b,z=c,avg=0.0;
+        avg=(x+y+z)/3.0;
+        return avg;
+
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 3 nos::");
+        double a=sc.nextDouble();
+        double b=sc.nextDouble();
+        double c=sc.nextDouble();
+
+        System.out.println("The average of 3 nos:"+checkAvearge(a, b, c));
+
+    }
+}
