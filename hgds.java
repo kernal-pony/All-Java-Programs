694905f3e36f333c9b15bc7122286bd4f6dee951
kernal-pony <67898649+kernal-pony@users.noreply.github.com>
2024-06-11 13:11:32 +0530

    delete hgds.java

--- hgds.java
+++ hgds.java
@@ -1,13 +1,1 @@
-// /*Input an email from the user. You have to create a username from the email by deleting the part that comes after ‘@’. Display that username to the user.
-// Example : 
-// email = “apnaCollegeJava@gmail.com” ; username = “apnaCollegeJava” 
-// email = “helloWorld123@gmail.com”; username = “helloWorld123”
-//  */
 
-// import java.util.*;
-// public class 
-// {
-//     public static void main(String args[])
-//     {
-//         Scanner sc = new Scanner (System.in);
-//         String original= "xyz" ;
